8d45179489c68d2945eaa320bd200844758175fe
dev2 <1570769179@qq.com>
2019-09-20 18:02:36 +0800

    fs

--- file.js
+++ file.js
@@ -2,5 +2,5 @@
  * Created by dev2 on 2019/9/20. node操作文件
  */
 var fs = require("fs");
-var data = fs.readFileSync("index.html");
+var data = fs.readFileSync("file.js");
 console.log(data.toString(),"同步读取");
