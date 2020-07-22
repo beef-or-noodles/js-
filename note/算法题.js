9761df8257fa74db6dd92edf4ec161156f11b5d8
wuwu <wuwanqiang@shigoo-china.com>
2020-07-22 18:04:23 +0800

    添加算法题

--- note/算法题.js
+++ note/算法题.js
@@ -6,10 +6,29 @@
 * 题目描述
 * 正整数A和正整数B 的最小公倍数是指 能被A和B整除的最小的正整数值，设计一个算法，求输入A和B的最小公倍数。
 * */
-class Computed {
-    constructor(){
-
+function f1(A,B) {
+    if(typeof A != 'number'&&typeof B != 'number') return '请输入正确的数据'
+    let arr = [] //用于存放合理值
+    let divisor=[1,1]
+    function fo(x,y){
+        let min =  x>y?x:y // 取得最小值
+        for (let i = 2; i <= min ;i++) {
+            if((x%i == 0)&&(y%i==0)){
+                arr.push(i)
+                divisor[0] = x/i
+                divisor[1] = y/i
+                fo(divisor[0],divisor[1])
+                return
+            }
+        }
     }
+    fo(A,B)
+    if(!arr.length){
+        arr = [A,B]
+    }
+    let num = arr.concat(divisor).reduce((total,cur)=>{
+        return cur * total
+    },1)
+   return num
 }
-
-var com = new Computed()
+console.log(f1(1,7));
