b9feeed97b0e3025f673bbe43258cb66107e63cf
wuwu <wuwanqiang@shigoo-china.com>
2020-08-07 17:11:31 +0800

    react路由

---
+++ React/react-blog/react-blog/src/pages/comment.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+export default class Comment extends React.Component {
+    render(){
+        return (
+            <h1>留言</h1>
+        )
+    }
+}
---
+++ React/react-blog/react-blog/src/pages/list.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+export default class List extends React.Component {
+    render(){
+        return(
+            <h1>列表页面</h1>
+        )
+    }
+}
