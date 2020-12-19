e0933662be7e2d4a86b4fa4db0acdc9787eff470
admin <1570769179@qq.com>
2020-12-19 15:57:30 +0800

    双色球数据更新

--- node爬虫/dispose.js
+++ node爬虫/dispose.js
@@ -5,7 +5,7 @@ const request = require('request')
 //文件模块
 let fs = require('fs');
 var path = require('path'); //系统路径模块
-var file = path.join(__dirname, './data.json'); //文件路径，__dirname为当前运行js文件的目录
+var file = path.join(__dirname, './reset.json'); //文件路径，__dirname为当前运行js文件的目录
 //var file = 'f:\\nodejs\\data\\test.json'; //也可以用这种方式指定路径
 
 //读取json文件
@@ -15,11 +15,24 @@ fs.readFile(file, 'utf-8', function(err, data) {
         res.send('文件读取失败');
     } else {
         let list = JSON.parse(data)
-        let total = list.length // 历史总期数
+        let res = []
+        list.forEach(item=>{
+            let result = JSON.parse(item)
+            result.result.forEach(ls=>{
+                res.push({
+                    time:ls.date,
+                    red:ls.red,
+                    bule:ls.blue,
+                    group:`${ls.red},${ls.blue}` // 组合
+                })
+            })
+        })
+        let total = res.length // 历史总期数
         let obj = {}
         let redObj = {}
         let redBall = {}
-        list.forEach(item=>{
+        let groupObj = {}
+        res.forEach(item=>{
             if(obj.hasOwnProperty(item.bule)){
                 obj[item.bule]+=1
             }else{
@@ -29,6 +42,12 @@ fs.readFile(file, 'utf-8', function(err, data) {
                 redObj[item.red]+=1
             }else{
                 redObj[item.red]=1
+            }
+            // 红蓝组合出现
+            if(groupObj.hasOwnProperty(item.group)) {
+                groupObj[item.group] += 1
+            }else{
+                groupObj[item.group] = 1
             }
             // 多次出现次数
             let arr = item.red.split(',')
@@ -68,6 +87,16 @@ fs.readFile(file, 'utf-8', function(err, data) {
                 value:redBall[key]
             })
         }
+        let groupArr = []
+        for(let key in groupObj){
+            groupArr.push({
+                label:key,
+                value:groupObj[key]
+            })
+        }
+        groupArr.sort((ls,el)=>{
+            return el.value - ls.value
+        })
 
         redArr.sort((ls,el)=>{
             return el.value - ls.value
@@ -80,18 +109,18 @@ fs.readFile(file, 'utf-8', function(err, data) {
         redBallArr.sort((ls,el)=>{
             return el.value - ls.value
         })
-        console.log(objArr);
-        console.log(redArr);
-        console.log(redBallArr);
-
-        let select = ['08',10,15,16,23,26] //01,06,09,14,19,22
-        let buleBall = '10'
+        console.log("红蓝组合历史出现排序",groupArr[0]);
+        console.log('红球组合出现次数排序',redArr[0]);
+        console.log('单个红球出现次数',redBallArr);
+        console.log('蓝球出现次数排序',objArr);
+        let select = ['03','09',13,23,26,27] //01,06,09,14,19,22
+        let buleBall = '12'
         let pitch = redArr.filter(item=>item.label == select.join(',')) // 组合出现机率
         let pitchBule = objArr.filter(item=>{return item.label == buleBall})
         let pitchRed = redBallArr.filter(item=>{return select.some(ls=> item.label == ls)})
 
         console.log(pitch);
-        console.log('红球组合历史出现次数',pitch);
+        console.log('红球组合历史出现次数',pitch,select);
         console.log('单个红球历史出现次数',pitchRed);
         console.log('单个蓝球历史出现次数',pitchBule)
 
--- node爬虫/request.js
+++ node爬虫/request.js
@@ -2,9 +2,22 @@
  * Created by admin on 2020/12/18.
  */
 
-var http = require('http');
+const request = require('request');
 httprequest('http://www.cwl.gov.cn/cwl_admin/kjxx/findDrawNotice?name=ssq&issueCount=&issueStart=&issueEnd=&dayStart=2003-10-01&dayEnd=2020-12-18&pageNo=2');
 
 function httprequest(url){
-
+    const options = {
+        url: url,
+        proxy:'http://218.89.76.103:80',
+        headers: {
+            'User-Agent': 'request',
+            'content-type': 'application/json'
+        }
+    };
+    request(options, (error, response, body)=>{
+        if (!error && response.statusCode == 200) {
+            const info = JSON.stringify(body);
+            console.log(info);
+        }
+    });
 };
