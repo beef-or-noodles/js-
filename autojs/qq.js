e54c8a1f33943f4156c56a311bf08636f0a2e9bc
readyWwq <1570769179@qq.com>
2021-01-07 23:54:39 +0800

    tijiao

--- autojs/qq.js
+++ autojs/qq.js
@@ -1,23 +1,26 @@
 
 //requestScreenCapture();
-// console.show()
+ console.show()
+ console.setPosition(0,1100)
+ console.setSize(device.width/2, 200);
 // 开启自动抢红包操作
+launchApp("微信")
+sleep(500)
 start()
 
 function start(){
        while(true){
             var dom = id('ga3').find()
-            console.log(dom.length);
+            sleep(1000)
+            console.log(dom.length,'检查未读消息')
             if(dom.length>0){
             let x = dom[0].bounds().centerX()
-            let y = dom[0].bounds().centerY() 
+            let y = dom[0].bounds().centerY()
             // 进入消息页面
-            sleep(200)
-            click(x-30,y+30)
+            click(x,y)
             // 判断是否是消息页面
             mesPage()
             }
-            sleep(2000)
       }
      
 }
@@ -25,36 +28,42 @@ function start(){
 
 // 消息页面操作
 function mesPage(){
+        sleep(300)
         var is_mes= id('g78').find()
-            console.log(is_mes.length,'消息');
+        console.log(is_mes.length,'是否消息页面');
         if(is_mes.length){
           // 执行红包判断
-          swipe(300, 300, 300, 1400, 1000)
-          
-         // rob()
+          //swipe(300, 300, 300, 1400, 1000)
+          rob()
         }else{
           // 点击返回 继续等待新消息
-          click(36,94)
+          back()
         }
 }
-
 // 抢红包操作
 function rob(){
-         var package = id('al7').find()
-         package.forEach((element,index) => {
-            package[index].click()
-            var open = desc('开').find()
-            if(open.length>0){
-               open[0].click()
-               sleep(500)         
-               var back = desc('返回').findOne()
-               click(back.bounds().centerX(), back.bounds().centerY())  
-            }else{
-              var back = desc('返回').findOne()
-              click(back.bounds().centerX(), back.bounds().centerY())  
-            }
+      var package = id('al7').find()
+      console.log(package.length,'页面红包个数');
+      if(package.length>0){
+         package[package.length-1].click()
+         sleep(500)
+         var open = desc('开').find()
+         console.log(open.length,'点击开启')
+         if(open.length>0){
+            open[0].click()
             sleep(500)
-         });
+            back()
+            back()
+         }else{
+            back()
+            back()
+         }
+      }else{
+         back()
+      }
+}
 
-         console.log(package.length,'红包个数');      
+function back(){
+   sleep(500)
+   click(36,94)
 }
