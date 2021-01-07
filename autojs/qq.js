0b1da0ff812912f5a2614d78fac9be4be1341344
admin <1570769179@qq.com>
2021-01-07 18:57:27 +0800

    微信抢抢红包

--- autojs/qq.js
+++ autojs/qq.js
@@ -2,6 +2,8 @@
 //requestScreenCapture();
 // console.show()
 // 开启自动抢红包操作
+start()
+
 function start(){
        while(true){
             var dom = id('ga3').find()
@@ -9,7 +11,6 @@ function start(){
             if(dom.length>0){
             let x = dom[0].bounds().centerX()
             let y = dom[0].bounds().centerY() 
-            console.log(x-30,y+30);
             // 进入消息页面
             sleep(200)
             click(x-30,y+30)
@@ -28,13 +29,15 @@ function mesPage(){
             console.log(is_mes.length,'消息');
         if(is_mes.length){
           // 执行红包判断
-            
+          swipe(300, 300, 300, 1400, 1000)
+          
+         // rob()
         }else{
           // 点击返回 继续等待新消息
           click(36,94)
         }
 }
-rob()
+
 // 抢红包操作
 function rob(){
          var package = id('al7').find()
@@ -43,6 +46,9 @@ function rob(){
             var open = desc('开').find()
             if(open.length>0){
                open[0].click()
+               sleep(500)         
+               var back = desc('返回').findOne()
+               click(back.bounds().centerX(), back.bounds().centerY())  
             }else{
               var back = desc('返回').findOne()
               click(back.bounds().centerX(), back.bounds().centerY())  
